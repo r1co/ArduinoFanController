8a0e0189f38921fe8a1d3dcc42d2036232b687f7
EW <r1co@post-box.cc>
2017-03-09 00:27:15 +0100

    cleanup + example

--- example/main.ino
+++ example/main.ino
@@ -1,10 +1,12 @@
 #include <FanController.h>
 
+const int fullSpeed = 255; // pwm value - default
+const int minSpeed = 75;   // pwm value - default
 const int powerPin = 8;
 
 FanController fanController = FanController(powerPin);
 
-void setup() {
+void example_setup() {
   // start serial port at 9600 bps:
   Serial.begin(9600);
 
@@ -12,9 +14,13 @@ void setup() {
   Fan fan1 = Fan(9);
 
   Fan fan2 = Fan(10);
+  fan2.setMinSpeed(minSpeed+20);
+  fan2.setMaxSpeed(fullSpeed-20);
 
   Fan fan3 = Fan(11);
-  
+  fan3.setStartUpDelay(5000); // fan needs to run longer at full speed befor slowing down
+
+
   fanController.addFan(fan1);
   fanController.addFan(fan2);
   fanController.addFan(fan3);
@@ -24,7 +30,7 @@ void setup() {
 
 }
 
-void loop() {
+void example_loop() {
   // run fan controller
   fanController.run();
 
